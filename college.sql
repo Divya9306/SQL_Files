bb54c4a62f26a2276693bc3bb7c2180ff41bb12a
unknown <divyakaur2005@gmail.com>
2026-06-21 23:12:56 +0530

    changes in college.sql

--- college.sql
+++ college.sql
@@ -1,3 +1,14 @@
 CREATE DATABASE college;
 
 USE college;
+
+USE college;
+
+CREATE TABLE ibm (id INT, name VARCHAR(20), salary INT, doj date);
+SELECT * FROM ibm;
+
+INSERT INTO ibm VALUES(01, 'Divya', 50000, '2026-06-30');
+INSERT INTO ibm VALUES(02, 'Divyansh', 59000, '2026-06-30');
+INSERT INTO ibm VALUES(03, 'Divyam', 40000, '2026-06-30');
+INSERT INTO ibm VALUES(04, 'Divyakriti', 11000, '2026-06-30');
+1bdc7a1 (changes in college.sql)
