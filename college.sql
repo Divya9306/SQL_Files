642ea14ec92f9b40386f4f325d358ad8cff3674b
unknown <divyakaur2005@gmail.com>
2026-06-21 23:31:21 +0530

    Update college.sql

--- college.sql
+++ college.sql
@@ -2,8 +2,6 @@ CREATE DATABASE college;
 
 USE college;
 
-USE college;
-
 CREATE TABLE ibm (id INT, name VARCHAR(20), salary INT, doj date);
 SELECT * FROM ibm;
 
@@ -11,4 +9,9 @@ INSERT INTO ibm VALUES(01, 'Divya', 50000, '2026-06-30');
 INSERT INTO ibm VALUES(02, 'Divyansh', 59000, '2026-06-30');
 INSERT INTO ibm VALUES(03, 'Divyam', 40000, '2026-06-30');
 INSERT INTO ibm VALUES(04, 'Divyakriti', 11000, '2026-06-30');
-1bdc7a1 (changes in college.sql)
+
+SELECT * FROM ibm ORDER BY id DESC;
+SELECT * FROM ibm ORDER BY SALARY DESC;
+
+SELECT * FROM ibm WHERE salary<=50000 and salary>=45000;
+
